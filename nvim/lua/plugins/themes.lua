04b0cfe5a46b8d1462f99bfec74f7dc7648a3102
kmrk <kmrkgo@gmail.com>
2023-05-24 20:57:28 +0800

    default lsp setting

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,19 +1,50 @@
 return {
-  { "folke/tokyonight.nvim", name = "tokyonight" },
-  { "catppuccin/nvim", name = "catppuccin" },
-  { "ellisonleao/gruvbox.nvim" },
-  { "patstockwell/vim-monokai-tasty" },
-  { "tanvirtin/monokai.nvim" },
+  -- { "folke/tokyonight.nvim", name = "tokyonight" },
+  -- { "catppuccin/nvim", name = "catppuccin" },
+  -- { "ellisonleao/gruvbox.nvim" },
+  -- { "patstockwell/vim-monokai-tasty" },
+  -- {
+  --   "tanvirtin/monokai.nvim",
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     --colorscheme = "vim-monokai-tasty",
+  --     -- colorscheme = "gruvbox",
+  --     --colorscheme = "catppuccin",
+  --     colorscheme = "monokai",
+  --     --colorscheme = "tokyonight",
+  --     --style = "moon",
+  --     ----style = "day", --"moon", -- "day" -- "night"
+  --   },
+  -- },
+  --
   {
-    "LazyVim/LazyVim",
+
+    -- "catppuccin/nvim",
+    -- "ellisonleao/gruvbox.nvim",
+    -- "folke/tokyonight.nvim",
+    "tanvirtin/monokai.nvim",
     opts = {
-      --colorscheme = "vim-monokai-tasty",
-      -- colorscheme = "gruvbox",
-      --colorscheme = "catppuccin",
-      colorscheme = "monokai",
-      --colorscheme = "tokyonight",
-      --style = "moon",
-      ----style = "day", --"moon", -- "day" -- "night"
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      background_colour = "#000000",
     },
   },
 }
