c32055d6f936dd7255a5f7517f87365c2433764c
kmrk <kmrkgo@gmail.com>
2023-07-17 00:07:59 +0800

    somecchange

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,23 +1,37 @@
-return 
-  {
-    "nvim-neo-tree/neo-tree.nvim",
+return {
+  "nvim-neo-tree/neo-tree.nvim",
 
-    opts = {
-      git_status = {
-        symbols = {
-          -- Change type
-          added     = "✚", -- NOTE: you can set any of these to an empty string to not show them
-          deleted   = "✖",
-          modified  = "",
-          renamed   = "✎",
-          -- Status type
-          untracked = "",
-          ignored   = "",
-          unstaged  = "8",
-          staged    = "",
-          conflict  = "",
+  opts = {
+    git_status = {
+      symbols = {
+        -- Change type
+        added = "✚", -- NOTE: you can set any of these to an empty string to not show them
+        deleted = "✖",
+        modified = "",
+        renamed = "✎",
+        -- Status type
+        untracked = "",
+        ignored = "",
+        unstaged = "8",
+        staged = "",
+        conflict = "",
+      },
+      align = "right",
+    },
+
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
         },
-        align = "right",
+        never_show = {},
       },
     },
-  }
+  },
+}
