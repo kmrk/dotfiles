5181429948b584cf2a524805fd921941bec222f6
ysong <ysong@139.com>
2024-06-20 22:43:58 +0800

    aa

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,22 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+
+-- if vim.g.fvim_loaded then
+if vim.g.vscode then -- do nothing config
+else
+  require("config.lazy")
+  vim.g.FVimCursorSmoothMove = true
+  vim.g.FVimCursorSmoothBlink = true
+  vim.filetype.add({ extension = { purs = "purescript" } })
+  vim.g.ayucolor = "mirage"
+  local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+  parser_config.fsharp = {
+    install_info = {
+      url = "https://github.com/Nsidorenco/tree-sitter-fsharp",
+      branch = "develop",
+      files = { "src/scanner.cc", "src/parser.c" },
+      generate_requires_npm = true,
+      requires_generate_from_grammar = true,
+    },
+    filetype = "fsharp",
+  }
+end
--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -5,7 +5,7 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
-vim.opt.guifont = { "JetBrainsMono Nerd Font Mono", ":h12" }
+vim.opt.guifont = { "JetBrains Mono:h8" }
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
@@ -14,7 +14,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
