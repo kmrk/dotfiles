c0a1503fce280550c4930e81557f063f027820f0
ysong <ysong@139.com>
2024-10-03 22:49:33 +0800

    aa

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -4,8 +4,8 @@ return {
   {
     "kdheepak/monochrome.nvim",
     config = function()
-      vim.cmd.colorscheme("monochrome")
       vim.opt.background = "light"
+      vim.cmd.colorscheme("monochrome")
       vim.cmd([[highlight Normal guibg=#f9f9f9]])
     end,
   },
@@ -13,17 +13,10 @@ return {
   {
     "sainnhe/sonokai",
     priority = 1000,
-
     config = function()
       vim.g.sonokai_enable_italic = false
       vim.g.sonokai_dim_inactive_windows = true
-      -- vim.g.sonokai_style = "atlantis"
       vim.g.sonokai_style = "maia"
-
-      --vim.g.sonokai_style = "shusia"
-      --vim.g.sonokai_style = "andromeda"
-      --vim.g.sonokai_style = "espresso"
-      --vim.cmd.colorscheme("github_light_tritanopia")
     end,
   },
 }
